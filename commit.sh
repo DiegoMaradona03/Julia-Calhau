f84bd99c6733d984f335c3be2bb2412ed9077884
Bolsolula <160658525+DiegoMaradona03@users.noreply.github.com>
2025-08-06 07:55:48 -0300

    Create commit.sh

---
+++ commit.sh
@@ -0,0 +1,21 @@
+```
+git config --global user.name "DiegoMaradona03"
+```
+```
+git config --global user.email "example@gmail.com"
+```
+```
+git pull
+```
+```
+git add .
+```
+```
+git commit -m "chamada"
+```
+```
+git push
+```
+```
+git config --list
+```
